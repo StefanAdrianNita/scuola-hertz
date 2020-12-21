f318faab4b916869a9e2ef361057f82a2c410e90
Stefan Adrian Nita <stefanadrian.nita02@gmail.com>
2020-12-21 23:48:18 +0100

    update navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,7 +5,7 @@ class Navbar extends Component {
     render() { 
         return (
             <div className="navbar">
-                <h1>Navbar</h1>
+                
             </div>
          );
     }
