3127b0d82a29f3bc3e481c54710e2af6fc85b1d7
Manuel Marongiu <50673709+Manuel4twenty@users.noreply.github.com>
2020-12-23 13:38:50 +0100

    Created Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import "../Css/Navbar.css";
+
+class Navbar extends Component{
+
+    render(){
+        return(
+            <div className="Navbar">
+                <div className="Navbar__logo">
+
+                </div>
+                <div className="Navbar__item">
+                    <a href="" className="Navbar__button">Home</a>
+                    <a href="" className="Navbar__button">Home</a>
+                </div>
+                    <div className="Navbar__cta">
+                        <div className="cta">
+
+                        </div>
+                    </div>
+                </div>
+            
+
+        );
+    }
+}
+
+export default Navbar;
