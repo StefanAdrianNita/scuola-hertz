3e9ea1614526d0d2c843ab70f3d5dcb4729e5d77
Manuel Marongiu <50673709+Manuel4twenty@users.noreply.github.com>
2021-01-08 14:32:24 +0100

    About modify

--- src/components/About.js
+++ src/components/About.js
@@ -49,7 +49,7 @@ function About(){
                     <h1>I nostri Insegnanti</h1>
                     <p>{TeacherInfo}</p>
                     <div className="teacher-btn">
-                        <Link>
+                        <Link to="/Teacher">
                             <Button text="I nostri Professori"></Button>
                         </Link>
                     </div>
